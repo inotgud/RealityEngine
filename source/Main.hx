5537944a8589e6592a4db2f975f830ce46b7153a
Good <85409973+Goodbs@users.noreply.github.com>
2022-11-23 01:35:23 +0300

    shit haxeflixel v2

--- source/Main.hx
+++ source/Main.hx
@@ -100,7 +100,7 @@ class Main extends Sprite
 		bitmapFPS.smoothing = true;
 		#end
 
-		game = new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen);
+		game = new FlxGame(gameWidth, gameHeight, initialState, #if (flixel < "5.0.0") zoom, #end framerate, framerate, skipSplash, startFullscreen);
 		addChild(game);
 
 		//custom cursor image
